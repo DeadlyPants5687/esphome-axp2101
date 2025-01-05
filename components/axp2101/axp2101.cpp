c6181b68246540733b27d371b6f60669d1d4c180
Kai Nissen <kai@kainissen.net>
2025-01-05 17:54:30 +0100

    add logging to UpdateBrightness()

--- components/axp2101/axp2101.cpp
+++ components/axp2101/axp2101.cpp
@@ -428,6 +428,7 @@ void AXP2101Component::UpdateBrightness()
     switch (this->model_) {
       case AXP2101_M5CORE2:
       {
+	ESP_LOGD(TAG, "Actually setting backlight brightness.");
         uint8_t buf = Read8bit( 0x27 );
         Write1Byte( 0x27 , ((buf & 0x80) | (ubri << 3)) );
         break;
